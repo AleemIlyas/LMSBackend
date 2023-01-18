ec235eefacaa44bc64ea5dde1d3def5ab198aea3
unknown <aleemempire215@gmail.com>
2023-01-18 11:10:03 +0500

    basic change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const communication = require('./src/routers//contactRouter')
 const admin = require('./src/routers/adminsRouter');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use( cors({
     origin : '*'
